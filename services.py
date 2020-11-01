b1da7e14b3b86b0721178e2f6d96e226ce501163
Mohammad Elham Amin <48331678+melhamin@users.noreply.github.com>
2020-11-01 22:08:48 +0300

    refactored the code

--- services.py
+++
@@ -1,9 +0,0 @@
-
-supported_providers = {
-    'google': ['imap.gmail.com', 993],
-    'bilkent': ['imap.bilkent.edu.tr', 587],
-    'yahoo': ['smtp.mail.yahoo.com', 465],
-    'outlook': ['outlook.office365.com', 465],
-}
-
-supported_browsers = ['chrome', 'firefox']
--- windows-installer.bat
+++ windows-installer.bat
@@ -7,6 +7,6 @@ python -m pip install --disable-pip-version-check virtualenv
 @echo [+] Creating a new venv...
 python -m venv venv
 @echo [+] Actiating venv...
-cmd /k "cd /d venv\Scripts & activate & cd /d ../../ & @echo [+] Installing the required packages... & pip install --disable-pip-version-check -r requirements.txt & @echo [+] Creating the executable... & pyinstaller --onefile main.py -n STARS --icon bilkent-logo.ico --log-level ERROR & @echo [+] Cleaning up... & cd /d venv\Scripts & deactivate & @echo [*] STARS WAS SUCCESSFULY INSTALLED! NOW YOU MAY CLOSE THIS WINDOW."
+cmd /k "cd /d venv\Scripts & activate & cd /d ../../ & @echo [+] Installing the required packages... & pip install --disable-pip-version-check -r requirements.txt & @echo [+] Creating the executable... & pyinstaller --onefile src/main.py -n STARS --icon src/bilkent-logo.ico --log-level ERROR & @echo [+] Cleaning up... & cd /d venv\Scripts & deactivate & cd /d ../../ & rmdir /S /Q build & rmdir /S /Q venv & del STARS.spec & @echo [*] STARS WAS SUCCESSFULY INSTALLED! NOW YOU MAY CLOSE THIS WINDOW."
 pause
 
