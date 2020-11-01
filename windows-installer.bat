5ac36034f0543ef2b02be31a49b7633df9f55370
Mohammad Elham Amin <48331678+melhamin@users.noreply.github.com>
2020-11-01 21:48:48 +0300

    fixed windows-installer error

--- windows-installer.bat
+++ windows-installer.bat
@@ -3,9 +3,10 @@
 @echo ############################################################
 
 @echo [+] Installing virtual env...
-python -m pip install --disable-pip-version-check -q virtualenv
+python -m pip install --disable-pip-version-check virtualenv
 @echo [+] Creating a new venv...
 python -m venv venv
 @echo [+] Actiating venv...
-cmd /k "cd /d venv\Scripts & activate & cd /d ../../ & @echo [+] Installing the required packages... & pip install --disable-pip-version-check -q -r requirements.txt & @echo [+] Creating the executable... & pyinstaller --onefile main.py --icon bilkent-logo.ico --log-level ERROR & @echo [+] Cleaning up... & cd /d venv\Scripts & deactivate & & @echo [*] STARS WAS SUCCESSFULY INSTALLED! NOW YOU MAY CLOSE THIS WINDOW."
+cmd /k "cd /d venv\Scripts & activate & cd /d ../../ & @echo [+] Installing the required packages... & pip install --disable-pip-version-check -r requirements.txt & @echo [+] Creating the executable... & pyinstaller --onefile main.py -n STARS --icon bilkent-logo.ico --log-level ERROR & @echo [+] Cleaning up... & cd /d venv\Scripts & deactivate & @echo [*] STARS WAS SUCCESSFULY INSTALLED! NOW YOU MAY CLOSE THIS WINDOW."
+pause
 
